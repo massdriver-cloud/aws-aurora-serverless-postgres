ccb07734a3aabc393d87fb3522084f5d9f26c31a
Cory O'Daniel <cory@coryodaniel.com>
2022-06-10 15:26:40 -0700

    new alarms

--- src/monitoring.tf
+++ src/monitoring.tf
@@ -13,8 +13,6 @@ module "database_capacity_alarm" {
 
   md_metadata         = var.md_metadata
   message             = "RDS Aurora ${aws_rds_cluster.main.cluster_identifier}: Serverless Database Capacity > ${local.database_capacity_percent * 100}% of Max"
-  alarm_sns_topic_arn = var.vpc.data.observability.alarm_sns_topic_arn
-
   alarm_name          = "${aws_rds_cluster.main.cluster_identifier}-highServerlessDatabaseCapacity"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = 1
